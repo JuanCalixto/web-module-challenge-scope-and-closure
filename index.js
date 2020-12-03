9a0c888107f16c7d6eab01f642d78c0250de3498
JuanCalixto <juancalixtomorales@gmail.com>
2020-12-02 21:46:41 -0600

    web-module-challenge-scope-and-closure

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
+  counter1 has count variable in in the local scope and counter2 has it in the global scope
   
   2. Which of the two uses a closure? How can you tell?
   
